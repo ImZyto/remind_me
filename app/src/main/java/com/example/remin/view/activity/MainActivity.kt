546f61475825e256642332b4e35871d1473ab720
PawelZielinski <pawezielinski7@gmail.com>
2022-06-23 18:44:25 +0200

    Setup Navigation Bar

--- app/src/main/java/com/example/remin/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/remin/view/activity/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.example.remin.view.activity
 
+import android.content.Context
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.remin.R
 import com.example.remin.presenter.activity.MainPresenter
 import com.example.remin.view.display.MainDisplay
 import kotlinx.android.synthetic.main.activity_main.*
+import org.osmdroid.config.Configuration
 
 class MainActivity : AppCompatActivity(), MainDisplay {
     private lateinit var navHost: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val ctx: Context = applicationContext
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
         setContentView(R.layout.activity_main)
-        navHost = findNavController(R.id.nav_main)
         MainPresenter(this)
     }
 
     override fun initBottomNavigation() {
-        val appBarConfiguration = AppBarConfiguration(setOf())
-        bottom_navigation.setupWithNavController(navHost)
+        val navController = findNavController(R.id.fragmentContainer)
+        bottom_navigation.setupWithNavController(navController)
     }
 }
